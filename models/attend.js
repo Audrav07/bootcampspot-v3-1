b2fa20c1ee6344057f9842ba5c4af17d238afe3b
Joshua Loudermilk <jdloude@gmail.com>
2018-03-06 20:41:12 -0700

    changes

--- models/attend.js
+++ models/attend.js
@@ -1,13 +1,13 @@
 module.exports = function(sequelize, DataTypes) {
     const Attend = sequelize.define("Attend", {
-        // created_at: {
-        // 	type: DataTypes.DATE,
-        // 	defaultValue: 'CURRENT_TIMESTAMP'
-        // },
-        // updated_at: {
-        // 	type: DataTypes.DATE,
-        // 	defaultValue: 'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
-        // }
+        created_at: {
+            type: DataTypes.DATE,
+            defaultValue: 'CURRENT_TIMESTAMP'
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+            defaultValue: 'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
+        }
     }, { underscored: true });
 
 
