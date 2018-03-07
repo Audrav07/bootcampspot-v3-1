5076a49fa45f088c8bc71de5b972127fe29d5cc9
Dominic <gonza362@gmail.com>
2018-03-07 01:33:30 -0700

    added login errors using only the js file

--- public/js/login.js
+++ public/js/login.js
@@ -2,22 +2,55 @@
 // initial page view
 // show sign in, hide sign up
 let viewSignIn = true;
+let userErr;
+let emailErr1;
+let emailErr2;
+let emailErrMatch;
+let loginErr = {
+  signIn: function() {
+    $("#signInBtn").after(`<div id="signIn-error" <h5 style="color:red">
+                                        <i class="mdl-color-text--red material-icons">error_outline
+                                        </i> Invalid email or password</h5></div>`);
+  },
+  signUp: {
+    email1: function() {
+      $("#signUp-email1").after(`<div id="signUp-error1" <h5 style="color:red">
+                                        <i class="mdl-color-text--red material-icons">error_outline
+                                        </i> Invalid email address</h5></div>`);
+    },
+    email2: function() {
+      $("#signUp-email2").after(`<div id="signUp-error2" <h5 style="color:red">
+                                <i class="mdl-color-text--red material-icons">error_outline
+                                </i> Invalid email address</h5></div>`);
+    },
+    emailMatch: function() {
+      $("#signUpBtn").after(`<div id="signUp-error3" <h5 style="color:red">
+                                  <i class="mdl-color-text--red material-icons">error_outline
+                                  </i> Emails don't match</h5></div>`);
+    },
+    dbErr: function() {
+      alert(
+        "SORRY, Our Database Developer is either drunk or a little too high.  Please notify your Student Success Manager of our error. :("
+      );
+    }
+  }
+};
 $(".signIn").show();
 $(".signUp").hide();
 $(document).on("click", ".signUpLink", toggleLoginView);
 $(document).on("click", ".signInLink", toggleLoginView);
 
 function toggleLoginView(event) {
-    event.preventDefault();
-    if (viewSignIn === true) {
-        $(".signIn").hide();
-        $(".signUp").show();
-        viewSignIn = false;
-    } else {
-        $(".signIn").show();
-        $(".signUp").hide();
-        viewSignIn = true;
-    }
+  event.preventDefault();
+  if (viewSignIn === true) {
+    $(".signIn").hide();
+    $(".signUp").show();
+    viewSignIn = false;
+  } else {
+    $(".signIn").show();
+    $(".signUp").hide();
+    viewSignIn = true;
+  }
 }
 
 //////////////////////////// Work on this
@@ -30,34 +63,43 @@ function toggleLoginView(event) {
 $(document).on("click", "#signInBtn", handleSignInBtn);
 
 function handleSignInBtn(event) {
-    event.preventDefault();
-    // get user email
-    let user = $("#signIn-email")
-        .val()
-        .trim();
-    // get user password
-    let password = $("#signIn-pass")
-        .val()
-        .trim();
-    console.log(user, password);
+  event.preventDefault();
+  // get user email
+  let user = $("#signIn-email")
+    .val()
+    .trim();
+  // get user password
+  let password = $("#signIn-pass")
+    .val()
+    .trim();
+  console.log(user, password);
 
-    let userinfo = {
-        user: user,
-        password: password
-    };
+  let userinfo = {
+    user: user,
+    password: password
+  };
 
-    //   send user info to login route/db to check info
-    $.post("/api/login", userinfo).then(function(res) {
-        if (res.status !== 200) {
-            console.log("Email or password does not match");
-            /////////////////////////// Need to add html to show user the error
-            // show message in html that the email or password does not match
-        } else {
-            // if user exists and the password matches,
-            //user should be loggined in on the back end and redirect to landing page
-            window.location.replace("/landing");
-        }
-    });
+  //   send user info to login route/db to check info
+  $.post("/api/login", userinfo).then(function(res) {
+    //   get error html if any
+    userErr = document.getElementById("signIn-error");
+    if (res.status !== 200) {
+      console.log("Invalid email or password<");
+      //   if there is not already an error shown
+      if (userErr == null) {
+        // show message in html that the email or password does not match
+        loginErr.signIn();
+      }
+    } else {
+      // if user exists and the password matches,
+      //  if error message exists, remove
+      if (userErr !== null) {
+        $("#signIn-error").remove();
+      }
+      //user should be loggined in on the back end and redirect to landing page
+      window.location.replace("/landing");
+    }
+  });
 }
 
 // ----- Sign - Up --------
@@ -65,75 +107,98 @@ function handleSignInBtn(event) {
 $(document).on("click", "#signUpBtn", handleSignUpBtn);
 
 function handleSignUpBtn(event) {
-    event.preventDefault();
-    // get user info
-    const first_name = $("#first-name")
-        .val()
-        .trim();
-    const last_name = $("#last-name")
-        .val()
-        .trim();
-    const email1 = $("#form-email1")
-        .val()
-        .trim();
-    const email2 = $("#form-email2")
-        .val()
-        .trim();
-    const password = $("#form-pass1")
-        .val()
-        .trim();
-    const userClass = $("#classSelector")
-        .val()
-        .trim();
+  event.preventDefault();
+  // get user info
+  const first_name = $("#first-name")
+    .val()
+    .trim();
+  const last_name = $("#last-name")
+    .val()
+    .trim();
+  const email1 = $("#form-email1")
+    .val()
+    .trim();
+  const email2 = $("#form-email2")
+    .val()
+    .trim();
+  const password = $("#form-pass1")
+    .val()
+    .trim();
+  const userClass = $("#classSelector")
+    .val()
+    .trim();
 
-    console.log(first_name, "firstname");
-    console.log(last_name, "lastname");
-    console.log(email1, "email1");
-    console.log(email2, "email2");
-    console.log(password, "pass");
-    console.log(userClass, "class");
+  console.log(first_name, "firstname");
+  console.log(last_name, "lastname");
+  console.log(email1, "email1");
+  console.log(email2, "email2");
+  console.log(password, "pass");
+  console.log(userClass, "class");
 
-    console.log(validateEmail(email1), "this is email input");
-    if (validateEmail(email1)) {
-        if (validateEmail(email2)) {
-            if (email1 === email2) {
-                console.log("valid emails and they match");
-                //ajax call
-                user = email1;
-                const userData = {
-                    first_name,
-                    last_name,
-                    user,
-                    password,
-                    userClass
-                };
-                //////////////////////// Need to chnage if sending multiple errors for different reasons
-                // post data for user to be added to database
-                // back end should check to see if user already exists and then add or send back error
-                $.post("/api/signup", userData).then(function(res) {
-                    if (!res.created_at) {
-                        ////////////////  error adding to database = show error in html
-                        console.log("something went wrong with database");
-                    } else {
-                        //   everything is good so redirect to landing page
-                        console.log("It got this far");
-                        window.location.replace("/landing");
-                    }
-                });
-            } else {
-                console.log("both valid emails but no match");
+  console.log(validateEmail(email1), "this is email input");
+  if (validateEmail(email1)) {
+    if (validateEmail(email2)) {
+      if (email1 === email2) {
+        console.log("valid emails and they match");
+        //ajax call
+        user = email1;
+        const userData = {
+          first_name,
+          last_name,
+          user,
+          password,
+          userClass
+        };
+        // post data for user to be added to database
+        // back end should check to see if user already exists and then add or send back error
+        $.post("/api/signup", userData).then(function(res) {
+          emailErr1 = document.getElementById("signUp-error1");
+          emailErr2 = document.getElementById("signUp-error2");
+          emailErrMatch = document.getElementById("signUp-error3");
+          if (!res.created_at) {
+            ////////////////  error adding to database = show error
+            console.log("something went wrong with database");
+            loginErr.signUp.dbErr();
+          } else {
+            //   everything is good so redirect to landing page &
+            //   remove any error messages if they exists
+            if (emailErrMatch !== null) {
+              $("#signUp-error3").remove();
             }
-        } else {
-            ////////////////////// show error in html
-            console.log("problem with emails");
+            if (emailErr2 !== null) {
+              $("#signUp-error2").remove();
+            }
+            if (emailErr1 !== null) {
+              $("#signUp-error1").remove();
+            }
+            console.log("Should redirect to landing page");
+            window.location.replace("/landing");
+          }
+        });
+      } else {
+        console.log("both valid emails but no match");
+        if (emailErrMatch == null) {
+          LoginErr.signUp.emailMatch();
         }
+      }
     } else {
-        //////////////////////// show error in html
-        console.log("problem with emails");
+      // show error in html
+      console.log("problem with email 2");
+      if (emailErr2 == null) {
+        loginErr.signUp.email2();
+      }
     }
+  } else {
+    // show error in html
+    console.log("problem with email 1");
+    if (emailErr1 == null) {
+      loginErr.signUp.email1();
+    }
+  }
 }
+
 // check to make sure email is valid - trust me, it works.
 function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
