f790cceea6ec643ca7629d00416cd9340b4a6e2c
Joshua Loudermilk <jdloude@gmail.com>
2018-03-08 18:33:38 -0700

    all the code compiled

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-require("./routes/controller.js")(app);
+require("./controller/controller.js")(app);
 
 const mediator = require('./mediator');
 console.log("starting Server");
